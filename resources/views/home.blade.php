d9f36345bd3f60fa61e7cb10f2054584b30d2a4a
msopiann <muhammadsopian452@gmail.com>
2023-11-09 21:14:13 +0700

    add asset and set homepage

---
+++ resources/views/home.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('TemplateVR/css/bootstrap.css') }}">
+    <link rel="stylesheet" href="{{ asset('TemplateVR/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+
+    <script src="{{ asset('TemplateVR/js/bootstrap.js') }}"></script>
+    <script src="{{ asset('TemplateVR/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/js/script.js') }}"></script>
+</head>
+
+<body class="vh-100 overflow-hidden">
+    <nav class="navbar fixed-top">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/"><img src="/assets/img/logo.png" alt="Logo PSTI"></a>
+        </div>
+    </nav>
+
+    <main>
+        <section class="w-100 vh-100 d-flex flex-column justify-content-center align-items-center text-white fs-1">
+            <h1>Welcome To</h1>
+            <p>Virtual Campus Tour</p>
+            <p>UPI Purwakarta</p>
+
+            <button type="button" class="btn btn-lg"><a href="#" class="text-white text-decoration-none">Start
+                    Tour</a></button>
+        </section>
+    </main>
+</body>
+
+</html>
